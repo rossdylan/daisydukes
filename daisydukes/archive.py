95a73bfce39c182ba2b406c1f632573e82ef4358
Ross Delinger <rossdylan@csh.rit.edu>
2013-03-12 17:05:53 -0400

    finished up the archive zip function which works beautifully

--- daisydukes/archive.py
+++ daisydukes/archive.py
@@ -13,16 +13,17 @@ def createArchiveName(name, version, ext):
 def getFilesList(packages):
     files_list = map(lambda f: "/".join(f.split(".")), packages)
     files_list.append("setup.py")
-    files_list.append("README*")
+    files_list.append("README")
     return files_list
 
 
-def ArchiveZip(proj_info):
+def ArchiveZip(proj_info, extra_files):
     aname = createArchiveName(
         proj_info['name'],
         proj_info['version'],
         'zip'
     )
     files_list = getFilesList(proj_info['packages'])
-    zip_command = "zip {} {} -x {} ".format(aname, " ".join(files_list), exclude)
+    files_list.extend(extra_files)
+    zip_command = "zip -r {} {} -x {} ".format(aname, " ".join(files_list), exclude)
     check_call(split(zip_command))
