f5d9bc51b0e5a2ac02208679a418d195dcbc0532
Ross <rossdylan@csh.rit.edu>
2013-03-12 16:37:45 -0400

    forgot to change the name in setup.py to daisydukes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ version = '0.0.1'
 requires = []
 
 
-setup(name='pythong',
+setup(name='daisydukes',
     version=version,
     description="Set up a minimal, yet comfortable structure \
         for a Python project",
